bbc64fe0d165acad7e5a0cb5def684f793e94c53
emmanishikawa <emmanishikawa@gmail.com>
2025-01-21 12:22:32 -0800

    created sign up, sign in, and log out functions, created simple front end page, fixed tailwind version error

--- src/app/auth.ts
+++ src/app/auth.ts
@@ -1,14 +1,19 @@
 import { Collection, MongoClient } from "mongodb";
 import { encodeBase32LowerCaseNoPadding, encodeHexLowerCase } from "@oslojs/encoding";
 import { sha256 } from "@oslojs/crypto/sha2";
+import { cache } from "react";
+import { cookies } from "next/headers";
+import { Argon2id } from "oslo/password";
+import { redirect } from 'next/navigation';
 
-const client = new MongoClient(process.env.MONGO_URI!);
-async() => {
+const client = new MongoClient('mongodb://localhost:3000');
+export async function dbConnect() {
   await client.connect();
 };
 
 const db = client.db();
 const sessions: Collection<Session> = db.collection("sessions");
+const users: Collection<User> = db.collection("users");
 
 export function generateSessionToken(): string {
 	const bytes = new Uint8Array(20);
@@ -76,9 +81,109 @@ export async function invalidateSession(sessionId: string): Promise<void> {
     await sessions.deleteOne({id: sessionId});
 }
 
+export async function setSessionTokenCookie(token: string, expiresAt: Date): Promise<void> {
+	const cookieStore = await cookies();
+	cookieStore.set("session", token, {
+		httpOnly: true,
+		sameSite: "lax",
+		secure: process.env.NODE_ENV === "production",
+		expires: expiresAt,
+		path: "/"
+	});
+}
+
+export async function deleteSessionTokenCookie(): Promise<void> {
+	const cookieStore = await cookies();
+	cookieStore.set("session", "", {
+		httpOnly: true,
+		sameSite: "lax",
+		secure: process.env.NODE_ENV === "production",
+		maxAge: 0,
+		path: "/"
+	});
+}
+
+export const getCurrentSession = cache(async (): Promise<SessionValidationResult> => {
+	const cookieStore = await cookies();
+	const token = cookieStore.get("session")?.value ?? null;
+	if (token === null) {
+		return { session: null, user: null };
+	}
+	const result = await validateSessionToken(token);
+	return result;
+});
+
+export async function signup(formData: FormData): Promise<any> {
+    'use server';
+    
+    const username = formData.get('username') as string;
+    const password = formData.get('password') as string;
+
+    const hashedPassword = await new Argon2id().hash(password);
+
+    try {
+        await dbConnect();
+        
+        const user = await users.insertOne({
+            id: username,
+            password: password
+        });
+
+        const token = generateSessionToken();
+        const session = createSession(token, username, password);
+        setSessionTokenCookie(
+            (await session).id,
+            (await session).expiresAt
+        );
+      } catch (e) {
+        console.log('error', e);
+        return {
+          error: 'An unknown error occurred',
+        };
+      }
+      return redirect('/');
+}
+
+
+export async function signin(formData: FormData){
+    'use server';
+ 
+    const username = formData.get('username') as string;
+    const password = formData.get('password') as string;
+
+    await dbConnect();
+    const existingUser = await users.findOne({ id: username });
+    const validPassword = await new Argon2id().verify(
+        existingUser!.password,
+        password
+    );
+
+    const token = generateSessionToken();
+    const session = createSession(token, username, password);
+    setSessionTokenCookie(
+        (await session).id,
+        (await session).expiresAt
+    );
+    return redirect('/');
+}
+export async function logout(): Promise<any>{
+    'use server';
+    const session = getCurrentSession();
+    if (!session) {
+    return {
+      error: 'Unauthorized',
+    };
+  }
+ 
+  await invalidateSession((await session).session!.id);
+  deleteSessionTokenCookie();
+  return redirect('/');
+}
+
 export type SessionValidationResult =
 	| { session: Session; user: User }
 	| { session: null; user: null };
+
 
 export interface User {
     id: string;
@@ -90,83 +195,3 @@ export interface Session {
     userPassword: string;
     expiresAt: Date;
 }
-
-/*
-import { MongoClient, Collection } from "mongodb";
-import { encodeBase32LowerCaseNoPadding, encodeHexLowerCase } from "@oslojs/encoding";
-import { sha256 } from "@oslojs/crypto/sha2";
-
-const client = new MongoClient("mongodb://localhost:3000");
-await client.connect();
-const db = client.db("./db");
-const sessions: Collection<SessionDocument> = db.collection("sessions");
-
-export function generateSessionToken(): string {
-	const bytes = new Uint8Array(20);
-	crypto.getRandomValues(bytes);
-	return encodeBase32LowerCaseNoPadding(bytes);
-}
-
-export async function createSession(token: string, userId: number): Promise<Session> {
-	const sessionId = encodeHexLowerCase(sha256(new TextEncoder().encode(token)));
-	const session: Session = {
-		id: sessionId,
-		userId,
-		expiresAt: new Date(Date.now() + 1000 * 60 * 60 * 24 * 30),
-	};
-
-	await sessions.insertOne({
-		_id: session.id,
-		userId: session.userId,
-		expiresAt: session.expiresAt,
-	});
-
-	return session;
-}
-
-export async function validateSessionToken(token: string): Promise<Session | null> {
-	const sessionId = encodeHexLowerCase(sha256(new TextEncoder().encode(token)));
-	const document = await sessions.findOne({ _id: sessionId });
-
-	if (!document) {
-		return null;
-	}
-
-	const session: Session = {
-		id: document._id,
-		userId: document.userId,
-		expiresAt: new Date(document.expiresAt),
-	};
-
-	if (Date.now() >= session.expiresAt.getTime()) {
-		await sessions.deleteOne({ _id: sessionId });
-		return null;
-	}
-
-	if (Date.now() >= session.expiresAt.getTime() - 1000 * 60 * 60 * 24 * 15) {
-		session.expiresAt = new Date(Date.now() + 1000 * 60 * 60 * 24 * 30);
-		await sessions.updateOne(
-			{ _id: session.id },
-			{ $set: { expiresAt: session.expiresAt } }
-		);
-	}
-
-	return session;
-}
-
-export async function invalidateSession(sessionId: string): Promise<void> {
-	await sessions.deleteOne({ _id: sessionId });
-}
-
-export interface Session {
-	id: string;
-	userId: number;
-	expiresAt: Date;
-}
-
-interface SessionDocument {
-	_id: string;
-	userId: number;
-	expiresAt: Date;
-}
-*/
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,30 @@
+import { redirect } from "next/navigation";
+import { generateSessionToken, getCurrentSession, logout, signup, signin, validateSessionToken } from "./auth";
+import type { NextRequest } from "next/server";
+
 export default async function Home() {
-  return (
-    <>
-      
-      <button >sign up</button>
-      <button>sign in</button>
-    </>
-  );
+
+  const { user } = await getCurrentSession();
+  if (user) {
+    return (
+      <>
+        <p>logged in as {user.id}</p>
+        <br></br>
+        <button formAction={logout}>log out</button>
+      </>
+    );
+  } else {
+    return (
+      <>
+        <form>
+          <label htmlFor="username"></label>
+          <input type="text"></input>
+          <label htmlFor="password"></label>
+          <input type="text"></input>
+        </form>
+        <button formAction={signup}>sign up</button>
+        <button formAction={signin}>sign in</button>
+      </>
+    );
+  }
 }
