aa8f161b2711fd12a9716457a92d4af741a22b66
hoz011 <hoz011@ucsd.edu>
2025-02-14 17:15:25 -0800

    Buzzlights testing

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,7 @@
 // app/page.tsx
 import { queryDatabase } from '@/api/query-database';
 import { parseProperties, Post } from '@/utils/parse-properties';
+import Buzzlight from './components/buzzlight';
 
 export default async function HomePage() {
   // Fetch and parse posts on the server
@@ -8,13 +9,33 @@ export default async function HomePage() {
   const posts: Post[] = parseProperties(database);
 
   return (
-    <div>
+    /*<div>
       <h1>My Notion Blog</h1>
       <ul>
         {posts.map(({ id, title }) => (
           <li key={id}>{title}</li>
         ))}
       </ul>
+    </div>    */
+    
+    <div className="flex flex-wrap justify-center gap-4 p-10">
+      <Buzzlight 
+        imageSrc="/bee.png" 
+        hoverSrc="/blackbox.jpg" 
+        alt="Hexagon Image 1"
+      />
+      <Buzzlight 
+        imageSrc="/bee.png" 
+        hoverSrc="/blackbox.jpg" 
+        alt="Hexagon Image 2"
+      />
+      <Buzzlight 
+        imageSrc="/bee.png" 
+        hoverSrc="/blackbox.jpg" 
+        alt="Hexagon Image 3"
+      />
     </div>
+
+
   );
 }
