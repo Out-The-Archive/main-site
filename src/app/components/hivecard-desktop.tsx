28ef9ebb7deae3be113b4ad097d9b655acbf087a
EdwardNew <pkarrows@gmail.com>
2025-02-17 01:01:55 -0800

    fix: housekeeping, restructured files, deleted unused assets

--- src/app/components/hivecard-desktop.tsx
+++ src/app/components/hivecard-desktop.tsx
@@ -1,68 +1,54 @@
-import Image from 'next/image';
-import '../hive.css';
-import React from 'react';
-import * as assets from '../assets/svg';
-import { Accordion, AccordionItem } from '@szhsin/react-accordion';
-export function Hivecard_Desktop ({ hive, desc }: HiveCardProps) {
-const iconKey = `${hive}_icon` as keyof typeof assets;
-  return (
-  <>
-  <AccordionItem 
-  header={
-    <div className='d-hive-card'>
-          <div className={`${hive} d-rectangle flex`}>
-            <div className='flex'>
-              <div className='d-card-icon'>{assets[iconKey]}</div>
-              <div className='d-hive-card-text justify-left'>{hive}</div>
-            </div>
-            <div className='flex flex-row ml-auto space-x-2'>
-              <div>{assets.down_chevron}</div>
-              <div className=''>
-                {assets.card_hole}
-              </div>
-              <div>
-                <Image className="d-pin" src={JSON.parse(JSON.stringify(require('../assets/detail-pin-'+hive+'.png')))} alt="pin"/>
-              </div>
-            </div>
-          </div>
-    </div>
-    }>
-      <div className={`${hive} accordion-inner`}>
-        <div className='accordion-text-box'>{desc}</div>
-      </div>
-    </AccordionItem>
-  </>
-  );
+import Image from "next/image";
+import "../hive.css";
+import React from "react";
+import * as assets from "../../../public/svg";
+import { Accordion, AccordionItem } from "@szhsin/react-accordion";
+export function Hivecard_Desktop({ hive, desc }: HiveCardProps) {
+    const iconKey = `${hive}_icon` as keyof typeof assets;
+    return (
+        <>
+            <AccordionItem
+                header={
+                    <div className="d-hive-card">
+                        <div className={`${hive} d-rectangle flex`}>
+                            <div className="flex">
+                                <div className="d-card-icon">
+                                    {assets[iconKey]}
+                                </div>
+                                <div className="d-hive-card-text justify-left">
+                                    {hive}
+                                </div>
+                            </div>
+                            <div className="flex flex-row ml-auto space-x-2">
+                                <div>{assets.down_chevron}</div>
+                                <div className="">{assets.card_hole}</div>
+                                <div>
+                                    <Image
+                                        className="d-pin"
+                                        src={JSON.parse(
+                                            JSON.stringify(
+                                                require("../assets/detail-pin-" +
+                                                    hive +
+                                                    ".png")
+                                            )
+                                        )}
+                                        alt="pin"
+                                    />
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                }
+            >
+                <div className={`${hive} accordion-inner`}>
+                    <div className="accordion-text-box">{desc}</div>
+                </div>
+            </AccordionItem>
+        </>
+    );
 }
-
 
 interface HiveCardProps {
-  hive: string;
-  desc: string;
+    hive: string;
+    desc: string;
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,87 +1,30 @@
-import Image from 'next/image';
-import './hive.css';
-import React from 'react';
-import * as assets from './assets/svg';
-import { Hivecard_Mobile } from './components/hivecard-mobile';
-import { Hivecard_Desktop } from './components/hivecard-desktop';
-import pin from './assets/detail-pin-blue.png';
-import { Accordion } from '@szhsin/react-accordion';
+import Image from "next/image";
+import "./hive.css";
+import React from "react";
+import * as assets from "../../public/svg";
+import { Hivecard_Mobile } from "./components/hivecard-mobile";
+import { Hivecard_Desktop } from "./components/hivecard-desktop";
+import pin from "./assets/detail-pin-blue.png";
+import { Accordion } from "@szhsin/react-accordion";
 export default async function Home() {
-  return (
-  <>
-  <div className='flex flex-row'>
-    <div>
-      <Hivecard_Mobile hive={'design'}/>
-    </div>
-    <div>
-      <Hivecard_Mobile hive={'media'}/>
-    </div>
-    <div>
-      <Hivecard_Mobile hive={'music'}/>
-    </div>
-  </div>
-  <Accordion>
-    <Hivecard_Desktop hive='design' desc="short description 1" />
-    <Hivecard_Desktop hive='media' desc="short description 2" />
-    <Hivecard_Desktop hive='music' desc="short description 3" />
-  </Accordion>
-    
-  </>
-  );
-}
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-/*import { redirect } from "next/navigation";
-import { generateSessionToken, getCurrentSession, logout, signup, signin, validateSessionToken } from "./auth";
-import type { NextRequest } from "next/server";
-
-export default async function Home() {
-
-  const { user } = await getCurrentSession();
-  if (user) {
     return (
-      <>
-        <p>logged in as {user.id}</p>
-        <br></br>
-        <button formAction={logout}>log out</button>
-      </>
+        <>
+            <div className="flex flex-row">
+                <div>
+                    <Hivecard_Mobile hive={"design"} />
+                </div>
+                <div>
+                    <Hivecard_Mobile hive={"media"} />
+                </div>
+                <div>
+                    <Hivecard_Mobile hive={"music"} />
+                </div>
+            </div>
+            <Accordion>
+                <Hivecard_Desktop hive="design" desc="short description 1" />
+                <Hivecard_Desktop hive="media" desc="short description 2" />
+                <Hivecard_Desktop hive="music" desc="short description 3" />
+            </Accordion>
+        </>
     );
-  } else {
-    return (
-      <>
-        <form>
-          <label htmlFor="username"></label>
-          <input type="text"></input>
-          <label htmlFor="password"></label>
-          <input type="text"></input>
-        </form>
-        <button formAction={signup}>sign up</button>
-        <button formAction={signin}>sign in</button>
-      </>
-    );
-  }
 }
-*/
