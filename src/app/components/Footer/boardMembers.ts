927e629a43c722107b7703ad2406a570dd55642a
EdwardNew <pkarrows@gmail.com>
2025-02-25 14:31:10 -0800

    feat: working footer on all breakpoints

--- src/app/components/Footer/boardMembers.ts
+++ src/app/components/Footer/boardMembers.ts
@@ -1,10 +1,14 @@
-export default [
-    "Jarenz",
-    "Elisa",
-    "Brandon",
-    "Long",
-    "David",
-    "Edward",
-    "Ashley",
-    "Hannah",
+import { BoardImageProps } from "./BoardImage";
+
+const boardMembers: BoardImageProps[] = [
+    { name: "Jarenz", jumpingScale: 1, jumpHeight: -5 },
+    { name: "Elisa", jumpingScale: 0.8, jumpHeight: -20 },
+    { name: "Brandon", jumpingScale: 1.5, jumpHeight: 10 },
+    { name: "Long", jumpingScale: 1.4, jumpHeight: 10 },
+    { name: "David", jumpingScale: 1.4, jumpHeight: 10 },
+    { name: "Edward", jumpingScale: 1, jumpHeight: -5 },
+    { name: "Ashley", jumpingScale: 1, jumpHeight: -20 },
+    { name: "Hannah", jumpingScale: 1.45, jumpHeight: 10 },
 ];
+
+export default boardMembers;
