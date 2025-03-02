02bcd32a548fb66673229b56c91b3b2f0e3b8fb8
nbchen98 <146898044+nbchen98@users.noreply.github.com>
2025-03-02 01:30:11 -0800

    landing page svg imports and partial container fixes

--- src/app/sections/Landing.tsx
+++ src/app/sections/Landing.tsx
@@ -2,25 +2,28 @@ import Swipeable from "../components/Swipeable";
 
 export default function Landing() {
     return (
-        <div className="flex items-start justify-between w-full h-screen">
-            <div className="w-1/4 flex flex-col items-start ml-12 justify-center h-full">
+        <div className="relative w-screen h-screen flex">
+            {/* Left Text Section */}
+            <div className="border flex flex-col items-start ml-20 justify-center h-full z-10">
                 <h1 className="font-larken text-[145px] font-bold">OTA</h1>
-                <h4 className="font-larken text-[32px] font-light">(out the archive)."</h4>
+                <h4 className="font-larken text-[32px] font-light">(out the archive).</h4>
                 <a href="#" className="border-2 rounded-3xl py-1 px-6 font-larken font-light">
                     buzz with us
                 </a>
             </div>
-            <div className="overflow-hidden relative h-full w-3/4 items-center border justify-center">
+
+            {/* Swipeable Items Container */}
+            <div className="border absolute top-0 left-0 w-full h-full flex items-center justify-center overflow-hidden">
                 <Swipeable
-                    image="/landing/record-player.png"
+                    image="/landing/record-player.svg"
                     alt=""
                     width={216}
                     height={174}
-                    top="4.25rem"
+                    top="10.25rem"
                     left="10%"
                 />
                 <Swipeable
-                    image="/landing/folder.png"
+                    image="/landing/folder.svg"
                     alt=""
                     width={180}
                     height={80}
@@ -28,7 +31,7 @@ export default function Landing() {
                     left="calc(10% + 11rem)"
                 />
                 <Swipeable
-                    image="/landing/Paper-clips.png"
+                    image="/landing/Paper-clips.svg"
                     alt=""
                     width={250}
                     height={95}
@@ -36,7 +39,7 @@ export default function Landing() {
                     left="calc(10% + 4rem)"
                 />
                 <Swipeable
-                    image="/landing/Envelope.png"
+                    image="/landing/Envelope.svg"
                     alt=""
                     width={180}
                     height={95}
@@ -44,7 +47,7 @@ export default function Landing() {
                     left="calc(10% + 4rem)"
                 />
                 <Swipeable
-                    image="/landing/Casset.png"
+                    image="/landing/Casset.svg"
                     alt=""
                     width={140}
                     height={55}
@@ -52,7 +55,7 @@ export default function Landing() {
                     left="10%"
                 />
                 <Swipeable
-                    image="/landing/Postcard.png"
+                    image="/landing/Postcard.svg"
                     alt=""
                     width={180}
                     height={95}
@@ -60,12 +63,28 @@ export default function Landing() {
                     left="calc(10% + 10rem)"
                 />
                 <Swipeable
-                    image="/landing/Polaroids.png"
+                    image="/landing/Polaroids.svg"
                     alt=""
                     width={170}
                     height={95}
                     top="0.5rem"
                     left="calc(10% + 9rem)"
+                />
+                <Swipeable
+                    image="/landing/sm-paintbrush.svg"
+                    alt=""
+                    width={216}
+                    height={174}
+                    top="10.25rem"
+                    left="10%"
+                />
+                <Swipeable
+                    image="/landing/bg-paintbrush.svg"
+                    alt=""
+                    width={216}
+                    height={174}
+                    top="10.25rem"
+                    left="10%"
                 />
             </div>
         </div>
