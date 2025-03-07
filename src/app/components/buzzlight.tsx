58a6968baf8cf9f2aeadc2e95e18fdba74c98c41
hoz011 <hoz011@ucsd.edu>
2025-03-07 13:00:19 -0800

    Add poster for Safari bug fix and imporve layout/styling

--- src/app/components/buzzlight.tsx
+++ src/app/components/buzzlight.tsx
@@ -1,10 +1,20 @@
 "use client";
+//import { useEffect, useRef } from "react";
 
 type BuzzlightProps = {
     videoSrc: string;
 };
 
 export default function BuzzlightComponent({ videoSrc }: BuzzlightProps) {
+
+    /*const videoRef = useRef<HTMLVideoElement>(null);
+
+    useEffect(() => {
+        if (videoRef.current) {
+            videoRef.current.load();
+        }
+    }, [videoSrc]);*/
+
     return (
         <div
             className="flex-none group relative overflow-hidden
@@ -33,15 +43,17 @@ export default function BuzzlightComponent({ videoSrc }: BuzzlightProps) {
                 muted
                 loop
                 playsInline
+                //autoPlay
+                poster="/jollibee_poster.png"
                 preload="auto"
                 className="absolute inset-0 w-full h-full object-cover transition-transform duration-300 group-hover:scale-105 animate-fadeIn"
             />
             {/* Hover Overlay */}
             <div className="absolute grid place-items-center inset-0 bg-black bg-opacity-50 opacity-0 group-hover:opacity-100 transition-opacity duration-300">
                 <div className="relative flex flex-col items-center text-center text-white">
-                    <p className="text-[24px] font-larken opacity-100">Jollibee</p>
+                    <p className="text-[24px] lg:text-[30px] font-larken opacity-100">Jollibee</p>
                     <div className="w-24 h-[2px] bg-white my-1"></div>
-                    <p className="text-[13px] font-larken">Mixed Media</p>
+                    <p className="text-[13px] lg:text-[20px] font-larken">Mixed Media</p>
                 </div>
             </div>
         </div>
